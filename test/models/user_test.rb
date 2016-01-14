b5e4056ed6e3f0a1b83452481a02995a4628a058
Jezreel <jezreel.go@gmail.com>
2016-01-14 11:09:44 -0800

    Revise field names

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,24 +3,24 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "User is not saved if first name not supplied" do
-    user1 = User.new
+    user1 = User.new(last_name: "Doe", email: "user1@example.com")
     assert_not user1.save
   end
 
   test "User is not saved if last name not supplied" do
-    user2 = User.new(firstname: "John", email: "user2@example.com")
+    user2 = User.new(first_name: "John", email: "user2@example.com")
     assert_not user2.save
   end
 
   test "User is not saved no email is supplied" do
-    user3 = User.new(firstname: "John", lastname: "Doe" )
+    user3 = User.new(first_name: "John", last_name: "Doe" )
     assert_not user3.save
   end
 
   test "User is not saved if supplied email is a duplicate" do
-    user4 = User.new(firstname: "Jane", lastname: "Doe", email: "user4@example.com")
+    user4 = User.new(first_name: "Jane", last_name: "Doe", email: "user4@example.com")
     user4.save
-    user5 = User.new(firstname: "Jane", lastname: "Doe", email: "user4@example.com")
+    user5 = User.new(first_name: "Jane", last_name: "Doe", email: "user4@example.com")
     assert_not user5.save
   end
 
