371235ae04e6c28a39c85e867365e5a21a835df2
Jezreel <jezreel.go@gmail.com>
2016-01-14 11:40:10 -0800

    Add user validations and revise tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,24 +4,24 @@ class UserTest < ActiveSupport::TestCase
 
   test "User is not saved if first name not supplied" do
     user1 = User.new(last_name: "Doe", email: "user1@example.com")
-    assert_not user1.save
+    assert_not(user1.valid?, "User valid without first name")
   end
 
   test "User is not saved if last name not supplied" do
     user2 = User.new(first_name: "John", email: "user2@example.com")
-    assert_not user2.save
+    assert_not(user2.valid?, "User valid without last name")
   end
 
   test "User is not saved no email is supplied" do
     user3 = User.new(first_name: "John", last_name: "Doe" )
-    assert_not user3.save
+    assert_not(user3.valid?, "User valid without email")
   end
 
   test "User is not saved if supplied email is a duplicate" do
     user4 = User.new(first_name: "Jane", last_name: "Doe", email: "user4@example.com")
     user4.save
     user5 = User.new(first_name: "Jane", last_name: "Doe", email: "user4@example.com")
-    assert_not user5.save
+    assert_not(user5.valid?, "User valid with duplicate email")
   end
 
 end
