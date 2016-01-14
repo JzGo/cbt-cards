6b926b3dd93871fd88575e7a948d557a7b353d26
Jezreel <jezreel.go@gmail.com>
2016-01-14 12:23:44 -0800

    Add card validations and correct filename

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ActiveRecord::Base
+  validates :emotions, presence: true
+  validates :thoughts, presence: true
+  validates :reflection, presence: true
+  validates :intensity, presence: true
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -3,22 +3,22 @@ require 'test_helper'
 class CardTest < ActiveSupport::TestCase
 
   test "Card is not saved without emotion entry" do
-    card1 = Card.new(thought: "here", reflect: "there", intensity: 3)
+    card1 = Card.new(thoughts: "here", reflection: "there", intensity: 3)
     assert_not(card1.valid?, "Card valid without emotion entry")
   end
 
   test "Card is not saved without thought entry" do
-    card2 = Card.new(emotion: "happy", reflect: "there", intensity: 3)
+    card2 = Card.new(emotions: "happy", reflection: "there", intensity: 3)
     assert_not(card2.valid?, "Card valid without emotion entry")
   end
 
   test "Card is not saved without reflect entry" do
-    card3 = Card.new(emotion: "happy", thought: "here", intensity: 3)
+    card3 = Card.new(emotions: "happy", thoughts: "here", intensity: 3)
     assert_not(card3.valid?, "Card valid without reflect entry")
   end
 
   test "Card is not saved without intensity entry" do
-    card4 = Card.new(emotion: "happy", thought: "here", reflect: "there")
+    card4 = Card.new(emotions: "happy", thoughts: "here", reflection: "there")
     assert_not(card4.valid?, "Card valid without intensity")
   end
 end
