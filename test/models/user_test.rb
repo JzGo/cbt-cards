0e827ae89890b5339f014f92b0ac0ea7f99365f8
Jezreel <jezreel.go@gmail.com>
2016-01-13 22:45:06 -0800

    Initial user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "user fails if no name" do
+  test "User is not saved if first name not supplied" do
     user1 = User.new
     assert_not user1.save
   end
+
+  test "User is not saved if last name not supplied" do
+    user2 = User.new(firstname: "John", email: "user2@example.com")
+    assert_not user2.save
+  end
+
+  test "User is not saved no email is supplied" do
+    user3 = User.new(firstname: "John", lastname: "Doe" )
+    assert_not user3.save
+  end
+
+  test "User is not saved if supplied email is a duplicate" do
+    user4 = User.new(firstname: "Jane", lastname: "Doe", email: "user4@example.com")
+    user4.save
+    user5 = User.new(firstname: "Jane", lastname: "Doe", email: "user4@example.com")
+    assert_not user5.save
+  end
+
 end
