df968389c8150749bd13f2fea378dec7ef5e1611
Jezreel <jezreel.go@gmail.com>
2016-01-15 12:17:21 -0800

    Add card factory and user with cards factory

---
+++ test/factories/cards.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :card do
+    emotions "happy"
+    intensity "10"
+    thoughts "so happy"
+    reflection "but not too happy"
+    user factory: :user
+  end
+end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:first_name) { |n| "first{n}"}
-    sequence(:last_name) { |n| "last{n}"}
-    sequence(:email) { |n| "user{n}@example.com"}
-    sequence(:password) { |n| "password{n}"}
+    sequence(:first_name) { |n| "first#{n}"}
+    sequence(:last_name) { |n| "last#{n}"}
+    sequence(:email) { |n| "user#{n}@example.com"}
+    sequence(:password) { |n| "password#{n}"}
+
+    factory :user_with_cards do
+      cards factory: :card
+    end
   end
 end
