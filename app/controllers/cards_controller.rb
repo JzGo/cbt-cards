81592c5873fa33440ce0a7e0c0343edcae59d381
Jezreel <jezreel.go@gmail.com>
2016-01-19 14:55:10 -0800

    Adjust card fields and add card controller

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,19 @@
+class CardsController < ApplicationController
+
+  def create
+    @card = Card.new(card_params)
+    raise params.inspect
+    if @card.save
+      redirect_to(:back)
+    else
+      redirect_to(:back)
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:emotions, :intensity, :thoughts, :reflection, :user_id)
+  end
+
+end
