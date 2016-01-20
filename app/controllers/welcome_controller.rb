355695d5102077c1b10db9a505bd4d4a9ab16e42
Jezreel <jezreel.go@gmail.com>
2016-01-20 00:39:03 -0800

    Reorganize view files to reflect app structure

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   include WelcomeHelper
   def index
     if current_user
-      @cards = current_user.cards
-      render '/users/index'
+      @cards = current_user.cards.sort_by(&:created_at).reverse
+      render 'dashboard'
     end
   end
 end
