711a6945a213ec3c37d1abceee4db5c94c85c7d5
Jezreel <jezreel.go@gmail.com>
2016-01-20 19:16:10 -0800

    Add settings form panel to dash and configure params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
     @cards = current_user.cards
     @recent = @cards.sorted_by(&created_at).reverse.limit(5)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :city, :password, :current_password, :password_confirmation)
 end
