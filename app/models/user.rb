0e2e7cea2933a2f521a16795b02da2f539e1a444
Jezreel <jezreel.go@gmail.com>
2016-01-26 14:44:13 -0800

    Correct user provider relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :followed_through, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy
   has_many :follower_through, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
-  has_many :followeds, through: :follower_through, source: :follower
-  has_many :followers, through: :followed_through, source: :followed
+  has_many :followeds, through: :follower_through, source: :followed
+  has_many :followers, through: :followed_through, source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
