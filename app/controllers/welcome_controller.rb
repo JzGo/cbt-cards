c020fcfd957d53e00249a0257e56a884d384ea14
Jezreel <jezreel.go@gmail.com>
2016-01-26 16:38:35 -0800

    Filter results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
       @cards = current_user.cards.sort_by(&:created_at).reverse
       @clients = current_user.followeds
       @client_search = User.search(params[:query])
-      @client = User.all || @client_search
+      @results = User.where(access: 0) || @client_search
       # raise params.inspect
       render 'dashboard'
     end
