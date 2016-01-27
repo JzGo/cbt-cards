66a3419805bf80ffcd097e7650610856dc505df2
Jezreel <jezreel.go@gmail.com>
2016-01-27 14:15:06 -0800

    Add destroy method

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,8 +2,19 @@ class RelationshipsController < ApplicationController
 
   def create
     Relationship.create(relationship_params)
+    redirect_to :back
   end
 
+  def destroy
+    target = Relationship.find_by(relationship_params)
+    if target
+      target.destroy
+      redirect_to :back
+    else
+      flash[:error] = "Relationship does not exist"
+      redirect_to :back
+    end
+  end
 
   private
 
